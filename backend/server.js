1ac70e0421d0cab791b963788c7385646c34c8b4
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-18 07:56:01 -0500

    Implemented route request for server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hellos");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+//Movie Routes
+app.use("/api/movies", require("./routes/movieRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
