5b2ea553da27224ca90814741ec7017d4caa3ffd
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-18 08:13:45 -0500

    Added middleware to server for error handling and add custom error handler for express

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Movie Routes
 app.use("/api/movies", require("./routes/movieRoutes"));
 
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
