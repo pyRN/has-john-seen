9c718970461cabdefe43ca761d38bd196fd41bfd
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-30 14:55:43 -0500

    Changed backend port to 5000

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
