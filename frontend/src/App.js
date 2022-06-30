901152fecaa9ad4acb628bb143cdc10a82272546
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-30 14:39:22 -0500

    Removed wrapper container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 //Components
 import LandingContainer from "./Components/LandingPage/LandingContainer";
-import MovieComponent from './Components/MoviePage/MovieComponent'
+import MovieComponent from "./Components/MoviePage/MovieComponent";
 import MoviesContainer from "./Components/MoviePage/MoviesContainer";
 import LoginContainer from "./Components/LoginPage/LoginContainer";
 import RegisterContainer from "./Components/LoginPage/RegisterContainer";
@@ -12,16 +12,14 @@ import ForgotContainer from "./Components/LoginPage/ForgotContainer";
 const App = () => {
   return (
     <BrowserRouter>
-      <main className="flex-column center container">
-        <Routes>
-          <Route path="/" element={<LandingContainer />} />
-          <Route path="/movies" element={<MoviesContainer />} />
-          <Route path="/movie" element={<MovieComponent/>} />
-          <Route path="/login" element={<LoginContainer />} />
-          <Route path="/register" element={<RegisterContainer />} />
-          <Route path="/forgot" element={<ForgotContainer />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<LandingContainer />} />
+        <Route path="/movies" element={<MoviesContainer />} />
+        <Route path="/movie" element={<MovieComponent />} />
+        <Route path="/login" element={<LoginContainer />} />
+        <Route path="/register" element={<RegisterContainer />} />
+        <Route path="/forgot" element={<ForgotContainer />} />
+      </Routes>
     </BrowserRouter>
   );
 };
