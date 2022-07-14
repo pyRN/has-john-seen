da45b6a492167b4ed4ea208538e0b2c52f28e879
Jacob Mayeux <jacobmayeux@gmail.com>
2022-07-14 17:03:34 -0500

    Added Navbar and Toast elements to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 //Components
 import LandingContainer from "./Components/LandingPage/LandingContainer";
 import MovieComponent from "./Components/MoviePage/MovieComponent";
 import MoviesContainer from "./Components/MoviePage/MoviesContainer";
+import Navbar from "./Components/Navbar/Navbar";
 import LoginContainer from "./Components/LoginPage/LoginContainer";
 import RegisterContainer from "./Components/LoginPage/RegisterContainer";
 import ForgotContainer from "./Components/LoginPage/ForgotContainer";
+import DashboardContainer from "./Components/Dashboard/DashboardContainer";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingContainer />} />
-        <Route path="/movies" element={<MoviesContainer />} />
-        <Route path="/movie" element={<MovieComponent />} />
-        <Route path="/login" element={<LoginContainer />} />
-        <Route path="/register" element={<RegisterContainer />} />
-        <Route path="/forgot" element={<ForgotContainer />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingContainer />} />
+          <Route path="/movies" element={<MoviesContainer />} />
+          <Route path="/movie" element={<MovieComponent />} />
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/register" element={<RegisterContainer />} />
+          <Route path="/forgot" element={<ForgotContainer />} />
+          <Route path="/dashboard" element={<DashboardContainer />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 };
 
